13bb935c0dbff9c27146167f6fc1058726b147d3
saasbook <saasbook@saasbook.(none)>
2013-02-03 06:08:17 -0800

    added session logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,25 +7,36 @@ class MoviesController < ApplicationController
   end
 
   def index
-     ratings = params[:ratings]
+     ratings = params[:ratings]!=nil ? params[:ratings] : session[:ratings]
+     filters = params[:filter]!=nil ? params[:filter] : session[:filter]
+     #sort = params[:sort]!=nil ? params[:sort] : session[:sort]
+
+     #ratings =  params[:ratings] 
+     #filters =  params[:filter] 
+     sort = params[:sort] 
+
+
      @all_ratings = Movie.getAvailableRatings(ratings)
-    if params[:sort]=="title"
-     if params[:filter]!=nil
-      @movies =Movie.where(:rating => params[:filter].select {|k,v| v=='true'}.keys ).sort_by &:title
-      @all_ratings = Movie.getAvailableRatings(params[:filter].select {|k,v| v=='true'})
+    if sort=="title"
+     if filters!=nil
+      @movies =Movie.where(:rating => filters.select {|k,v| v=='true'}.keys ).sort_by &:title
+      @all_ratings = Movie.getAvailableRatings(filters.select {|k,v| v=='true'})
      else
       @movies=Movie.all.sort_by &:title
      end
-    elsif params[:sort]=="created"
-     if params[:filter]!=nil
-      @movies =Movie.where(:rating => params[:filter].select {|k,v| v=='true'}.keys).order("release_date ASC")
-      @all_ratings = Movie.getAvailableRatings(params[:filter].select {|k,v| v=='true'})
+    elsif sort=="created"
+     if filters!=nil
+      @movies =Movie.where(:rating => filters.select {|k,v| v=='true'}.keys).order("release_date ASC")
+      @all_ratings = Movie.getAvailableRatings(filters.select {|k,v| v=='true'})
      else
       @movies = Movie.find :all, :order => "release_date ASC"    
      end    
     else
      @movies = ratings==nil ? Movie.all : Movie.where(:rating => ratings.keys) 
     end
+    session[:ratings] = ratings == nil ? session[:ratings] : ratings
+    session[:sort] = params[:sort] == nil ? session[:sort] : params[:sort]
+    session[:filter] = params[:filter] == nil ? session[:filter] : params[:filter]
   end
 
   def new
